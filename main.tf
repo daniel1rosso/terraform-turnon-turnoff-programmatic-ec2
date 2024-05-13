fa2d37897f575bce051cb1c79a659cbddc9743e6
Daniel Rosso <38544140+daniel1rosso@users.noreply.github.com>
2024-05-13 14:36:21 -0300

    Update project

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-//---- LAMBDAS ----//
+//---- Lambdas ----//
 data "archive_file" "lambda_turnon_turnoff_ec2_zip" {
   for_each    = var.lambda_code_turnon_turnoff_ec2
   type        = "zip"
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "lambda_turnon_turnoff_ec2" {
     aws_iam_role.lambda_turnon_turnoff_ec2_role
   ]
   for_each         = var.lambda_turnon_turnoff_ec2
-  function_name    = each.value["function_name"]
+  function_name    = each.value.function_name
   description      = each.value["description"]
   role             = aws_iam_role.lambda_turnon_turnoff_ec2_role.arn
   filename         = each.value["filename"]
@@ -21,56 +21,88 @@ resource "aws_lambda_function" "lambda_turnon_turnoff_ec2" {
   timeout          = each.value["timeout"]
   memory_size      = each.value["memory_size"]
   architectures    = [each.value["architectures"]]
+  tags = {
+    Environment = var.tags["env"]
+    project     = var.tags["project"]
+  }
 }
 
 //---- Role ----//
-
 resource "aws_iam_role" "lambda_turnon_turnoff_ec2_role" {
-  name               = "lambda-turnon-turnoff-ec2-role-${var.region}"
-  assume_role_policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"                 
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
-POLICY
+  name = "lambda-turnon-turnoff-ec2-role-${var.region}"
+  assume_role_policy = templatefile("./iam/roles/lambda_turnon_turnoff_ec2.json", {
+  })
 }
 
 //---- Custom Policies ----//
-
 resource "aws_iam_policy" "lambda_start_stop_ec2_policy" {
   name = "access_start_stop_ec2_${var.region}"
-  policy = templatefile("./resources/policy/lambda-stop-start-ec2.json", {
+  policy = templatefile("./iam/policies/lambda_turnon_turnoff_ec2.json", {
   })
 }
 
 //---- Attach Policies to Role ----//
-
-resource "aws_iam_role_policy_attachment" "lambda_campaigns_policy_attach5" {
+resource "aws_iam_role_policy_attachment" "lambda_start_stop_policy_attach" {
+  depends_on = [
+    aws_iam_role.lambda_turnon_turnoff_ec2_role,
+    aws_iam_policy.lambda_start_stop_ec2_policy
+  ]
   role       = aws_iam_role.lambda_turnon_turnoff_ec2_role.name
   policy_arn = aws_iam_policy.lambda_start_stop_ec2_policy.arn
 }
 
-//---- Event Bridge ----//
-
-resource "aws_cloudwatch_event_rule" "eventbridge_lambda_start_stop_ec2" {
-  name                = "eventbridge_lambda_start_stop_ec2_${var.region}"
-  schedule_expression = "cron(0 8,20 * * ? *)"
+resource "aws_iam_role_policy_attachment" "lambda_execute_policy_attach" {
+  role       = aws_iam_role.lambda_turnon_turnoff_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
 }
 
-resource "aws_cloudwatch_event_target" "eventbridge_lambda_start_stop_ec2_target" {
+resource "aws_iam_role_policy_attachment" "lambda_service_policy_attach" {
+  role       = aws_iam_role.lambda_turnon_turnoff_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaRole"
+}
+
+//---- Event Bridge ----//
+resource "aws_scheduler_schedule" "lambda_turnon_turnoff_ec2" {
   depends_on = [
-    aws_lambda_function.lambda_turnon_turnoff_ec2
+    aws_iam_role.lambda_turnon_turnoff_ec2_role,
+    aws_iam_policy.eventbridge_scheduler_policy
   ]
+
   for_each = aws_lambda_function.lambda_turnon_turnoff_ec2
-  rule     = aws_cloudwatch_event_rule.eventbridge_lambda_start_stop_ec2.name
-  target_id = "eventbridge_lambda_start_stop_ec2_target_${each.key}"
-  arn       = each.value.arn
+
+  name       = "lambda_turnon_turnoff_ec2_${each.value.function_name}"
+  group_name = "default"
+
+  # Set flexible_time_window to OFF to avoid conflicts
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  # Use cron expression for specific times
+  schedule_expression = "cron(0 11,23 * * ? *)"
+
+  target {
+    arn      = each.value.arn
+    role_arn = aws_iam_role.eventbridge_scheduler_role.arn
+  }
+}
+
+//---- Role ----//
+resource "aws_iam_role" "eventbridge_scheduler_role" {
+  name = "lambda-turnon-turnoff-ec2-role-eventbridge-${var.region}"
+  assume_role_policy = templatefile("./iam/roles/lambda_turnon_turnoff_eventbridge.json", {
+  })
+}
+
+//---- Custom Policies ----//
+resource "aws_iam_policy" "eventbridge_scheduler_policy" {
+  name = "lambda-turnon-turnoff-ec2-policy-eventbridge-${var.region}"
+  policy = templatefile("./iam/policies/lambda_turnon_turnoff_eventbridge.json", {
+  })
+}
+
+//---- Attach Policies to Role ----//
+resource "aws_iam_role_policy_attachment" "eventbridge_scheduler_policy_attach" {
+  role       = aws_iam_role.eventbridge_scheduler_role.name
+  policy_arn = aws_iam_policy.eventbridge_scheduler_policy.arn
 }
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -4,8 +4,8 @@ region = "us-east-1"
 //---- Lambda ----//
 lambda_code_turnon_turnoff_ec2 = {
   turnon_turnoff_ec2 = {
-    source_file = "/resources/lambda/instances_turnon_turnoff_ec2.py"
-    output_path = "/resources/lambda/instances_turnon_turnoff_ec2.zip"
+    source_file = "/code/lambda/instances_turnon_turnoff_ec2.py"
+    output_path = "/code/lambda/zip/instances_turnon_turnoff_ec2.zip"
   }
 }
 
@@ -13,11 +13,11 @@ lambda_turnon_turnoff_ec2 = {
   turnon_turnoff_ec2 = {
     function_name = "turnon_turnoff_ec2"
     description   = "turnon turnoff a ec2"
-    filename      = "./resources/lambda/instances_turnon_turnoff_ec2.zip"
-    handler       = "turnon_turnoff_ec2.lambda_handler"
+    filename      = "./code/lambda/zip/instances_turnon_turnoff_ec2.zip"
+    handler       = "instances_turnon_turnoff_ec2.lambda_handler"
     runtime       = "python3.10"
     timeout       = "900"
-    memory_size   = "256"
+    memory_size   = "128"
     architectures = "x86_64"
   }
 }
